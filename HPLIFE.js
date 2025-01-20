03743c6d653f091c469a7a6e4a252857e3aeb270
njagua <njagua.gma@gmail.com>
2025-01-20 20:27:39 +0300

    agile intro

---
+++ HPLIFE.js
@@ -0,0 +1,70 @@
+/* AGILE PROJECT MANAGEMENT
+
+Agile Methodologies
+
+In the past project management relied on the waterfall method.
+
+This method is based on detailed upfront planning$ phase execution.
+It worked well projects that had fixed goals $ clear requirements.
+
+1980's Agile Methodologies were developed by Hirotaka Takeuchi $ Nonaka.
+
+It focused on delivery increment $ ability  to adapt quickly tochanging requirements.
+
+Agile encourages innovation & improves collaboration among team members. 
+
+It aslo allows plan modification . This helps to overcome unexpected challenges and take
+advantage of opportunities as they come.
+
+The transition to agile is about embracing change and ensuring we can easily adjust
+to new demands.
+
+Being open to change is a strength and not a problem.
+
+Agile prioritizes delivering tangible value and continous improvement.
+It is built on collaboration, trust, & respect for those who do the work.
+
+Benefits of Agile Methodologies;
+-Collaboration
+-Trust & respect
+-Flexibility
+-Deliver continous value
+-Nurture improvement & growth.
+
+
+When developing a product, service or customer experience we usually start with building
+the MVP(MINIMUM VIABLE PRODUCT)
+
+MVP is not an end product but a baseline that reflects the core functionality enabling
+us to leanr from user feedback.
+
+With agile we can develop a new product in both an ITERATIVE and INCREMENTAL process.
+
+AGILE DEVELOPMENT METHODS
+
+1. ITERATIVE PRODUCT DEVELOPMENT - 
+
+Start with a draft . Increase details as we go along. 
+
+We do not have to finish the product in mind as we can change it as we develop.
+
+At every stage a client will receive something usable even if it does not match their needs
+
+In short we can deliver apps using MVP CONCEPT to showcase at different stages to user.
+
+STEPS:
+A- Start with draft
+B- Create a prototype on paper
+C- Have something functional
+D- As time goes by add more features.
+
+2. INCREMENTAL PRODUCT DEVELOPMENT
+
+This creating one piece at a time. eg interface, buttons, etc
+
+We usually have the full idea formulated before development.
+
+
+
+
+*/
